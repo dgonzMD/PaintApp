6b7d53a9ecf5a5acb898669b893bd77a28dbda5e
Daniel Gonzalez <dgonz14@gmail.com>
2012-05-30 13:29:17 -0400

    non-working fill

--- Globals.cs
+++ Globals.cs
@@ -7,6 +7,7 @@ using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
 namespace PaintApp
@@ -14,5 +15,7 @@ namespace PaintApp
     public static class Globals
     {
         public static SolidColorBrush scb;
+        public static int[] di = { -1, -1, -1, 0, 1, 1, 1, 0 };
+        public static int[] dj = { -1, 0, 1, 1, 1, 0, -1, -1 };
     }
 }
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 
@@ -18,7 +19,10 @@ namespace PaintApp
         private Point currentPoint;
         private Point oldPoint;
         private Line line;
-
+        private bool fill;
+        private bool[,] seen;
+        private Color fillColor;
+        private WriteableBitmap bm;
         // Constructor
         public MainPage()
         {
@@ -26,12 +30,13 @@ namespace PaintApp
             this.canvas1.MouseMove += new MouseEventHandler(canvas1_MouseMove);
             this.canvas1.MouseLeftButtonDown += new MouseButtonEventHandler(canvas1_MouseLeftButtonDown);
             Globals.scb = new SolidColorBrush(Colors.Black);
+            bm = new System.Windows.Media.Imaging.WriteableBitmap(rectangle1, null);
+            fill = false;
         }
 
         void canvas1_MouseMove(object sender, MouseEventArgs e)
         {
             currentPoint = e.GetPosition(this.canvas1);
-
             line = new Line() { X1 = currentPoint.X, Y1 = currentPoint.Y, X2 = oldPoint.X, Y2 = oldPoint.Y };
             line.Stroke = Globals.scb;
             line.StrokeThickness = 8;
@@ -39,39 +44,23 @@ namespace PaintApp
 
             this.canvas1.Children.Add(line);
             oldPoint = currentPoint;
+            fill = false;
         }
 
         void canvas1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             currentPoint = e.GetPosition(canvas1);
-            oldPoint = currentPoint;
+            oldPoint = currentPoint;         
         }
-
-        private void button1_Click(object sender, RoutedEventArgs e)
+        private void canvas1_Tap(object sender, GestureEventArgs e)
         {
-            Globals.scb = new SolidColorBrush(Colors.Blue);
+            if (fill)
+            {
+                fillColor = bm.GetPixel((int)currentPoint.X, (int)currentPoint.Y);
+                seen = new bool[bm.PixelWidth, bm.PixelHeight];
+                flood(currentPoint);
+            }
         }
-
-        private void button2_Click(object sender, RoutedEventArgs e)
-        {
-            Globals.scb = new SolidColorBrush(Colors.Green);
-        }
-
-        private void button3_Click(object sender, RoutedEventArgs e)
-        {
-            Globals.scb = new SolidColorBrush(Colors.Red);
-        }
-
-        private void button4_Click(object sender, RoutedEventArgs e)
-        {
-            Globals.scb = new SolidColorBrush(Colors.Yellow);
-        }
-
-        private void button5_Click(object sender, RoutedEventArgs e)
-        {
-            Globals.scb = new SolidColorBrush(Colors.Black);
-        }
-
         private void button1_Click_1(object sender, RoutedEventArgs e)
         {
             this.canvas1.Children.Clear();
@@ -85,6 +74,28 @@ namespace PaintApp
         private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
         {
             button2.Background = Globals.scb;
+        }
+        private void fillButton_Click(object sender, RoutedEventArgs e)
+        {
+            fill = true;
+        }
+        private void flood(Point p)
+        {
+            int x;
+            int y;
+            for (int i = 0; i < 8; i++)
+                for (int j = 0; j < 8; j++)
+                {
+                    x = (int)p.X + Globals.di[i];
+                    y = (int)p.Y + Globals.dj[j];
+                    if (x > 0 && x < bm.PixelWidth && y > 0 && y < bm.PixelHeight)
+                        if (!seen[x, y])
+                            if (fillColor == bm.GetPixel(x, y))
+                            {
+                                seen[x, y] = true;
+                                flood(new Point(p.X + Globals.di[i], p.Y + Globals.dj[j]));
+                            }
+                }
         }
     }
 }
