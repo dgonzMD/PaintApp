efa60123a0dc1304fe2d7dde6849d37b7a969a12
Frank <frankrod44@gmail.com>
2012-06-05 00:10:49 -0400

    switched to a black canvas, looks sexy now

--- Globals.cs
+++ Globals.cs
@@ -14,7 +14,7 @@ namespace PaintApp
 {
     public static class Globals
     {
-        public static SolidColorBrush scb= new SolidColorBrush(Colors.Black);
+        public static SolidColorBrush scb= new SolidColorBrush(Colors.White);
         public static int brushSize= 8;
         public static PenLineCap plc= PenLineCap.Round;
     }
